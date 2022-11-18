6d2200e13f5d7f7f9ba68a5624f2036d36399229
xabbok255 <xabbok255@ya.ru>
2022-11-18 11:54:04 +0300

    added MVVM, moved MainActivity to presentation folder, FormatNumber to utils folder

---
+++ app/src/main/java/ru/netology/nmedia/data/repository/PostRepositoryInMemoryImpl.kt
@@ -0,0 +1,38 @@
+package ru.netology.nmedia.data.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.netology.nmedia.domain.repository.PostRepository
+import ru.netology.nmedia.dto.Post
+
+class PostRepositoryInMemoryImpl : PostRepository {
+    private var post = Post(
+        id = 1,
+        author = "Нетология. Университет интернет-профессий будущего",
+        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → https://is.gd/xqtzIE",
+        published = "21 мая в 15:28",
+        likedByMe = false,
+        likes = 1399,
+        shares = 15999
+    )
+
+    private val data = MutableLiveData(post)
+
+    override fun get(): LiveData<Post> {
+        return data
+    }
+
+    override fun like() {
+        post = post.copy(
+            likedByMe = !post.likedByMe,
+            likes = post.likes + if (post.likedByMe) 1 else -1
+        )
+        data.value = post
+    }
+
+    override fun share() {
+        post = post.copy(shares = post.shares + 10)
+        data.value = post
+    }
+
+}
