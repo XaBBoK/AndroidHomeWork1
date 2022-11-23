2ef9eba69e8fb0c43a6a967946c0b53ab76678d2
xabbok255 <xabbok255@ya.ru>
2022-11-23 12:56:26 +0300

    change PostRepositoryInMemoryImpl data to List

--- app/src/main/java/ru/netology/nmedia/data/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/data/repository/PostRepositoryInMemoryImpl.kt
@@ -6,26 +6,55 @@ import ru.netology.nmedia.domain.repository.PostRepository
 import ru.netology.nmedia.dto.Post
 
 class PostRepositoryInMemoryImpl : PostRepository {
-    private var post = Post(
-        id = 1,
-        author = "Нетология. Университет интернет-профессий будущего",
-        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → https://is.gd/xqtzIE",
-        published = "21 мая в 15:28",
-        likedByMe = false,
-        likes = 999,
-        shares = 15999
+    private var posts = listOf(
+        Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Знаний хватит на всех: на следующей неделе разбираемся с чем-то еще",
+            published = "15 окт в 15:28",
+            likedByMe = false,
+            likes = 999,
+            shares = 2999,
+        ),
+        Post(
+            id = 2,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → https://is.gd/xqtzIE",
+            published = "21 мая в 15:28",
+            likedByMe = false,
+            likes = 1399,
+            shares = 15999,
+        ),
+        Post(
+            id = 3,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → https://is.gd/xqtzIE",
+            published = "21 мая в 15:28",
+            likedByMe = false,
+            likes = 1399,
+            shares = 15999,
+        ),
+        Post(
+            id = 4,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → https://is.gd/xqtzIE",
+            published = "21 мая в 15:28",
+            likedByMe = false,
+            likes = 1399,
+            shares = 15999,
+        )
     )
 
-    private val data = MutableLiveData(post)
+    private val data = MutableLiveData(posts)
 
-    override fun get(): LiveData<Post> {
+    /*override fun get(): LiveData<Post> {
         return data
-    }
+    }*/
 
-    override fun like() {
+    /*override fun like() {
         post = post.copy(
             likedByMe = !post.likedByMe,
-            likes = post.likes + if (post.likedByMe) -1 else 1
+            likes = post.likes + if (post.likedByMe) 1 else -1
         )
         data.value = post
     }
@@ -33,6 +62,24 @@ class PostRepositoryInMemoryImpl : PostRepository {
     override fun share() {
         post = post.copy(shares = post.shares + 10)
         data.value = post
+    }*/
+
+    override fun getAll(): LiveData<List<Post>> {
+        return data
+    }
+
+    override fun likeById(id: Long) {
+        posts = posts.map {
+            if (it.id == id)
+                it.copy(
+                    likedByMe = !it.likedByMe,
+                    likes = it.likes + if (it.likedByMe) -1 else 1
+                )
+            else
+                it
+        }
+
+        data.value = posts
     }
 
 }
