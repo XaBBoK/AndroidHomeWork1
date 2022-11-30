930a0c1dc69a95d4b142733b53c5dafa6d0b9d14
xabbok255 <xabbok255@ya.ru>
2022-11-30 13:15:12 +0300

    changed default values and added NON_EXISTING_POST_ID constant

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia.dto
 
+const val NON_EXISTING_POST_ID = -1L
+
 data class Post(
-    val id: Long,
-    val author: String,
-    val content: String,
-    val published: String,
+    val id: Long = NON_EXISTING_POST_ID,
+    val author: String = "",
+    val content: String = "",
+    val published: String = "",
     var likedByMe: Boolean = false,
     var likes: Int = 0,
     var shares: Int = 0
