93cd99465c849ea313449a9b4ca7d0fe04284590
xabbok255 <xabbok255@ya.ru>
2022-12-16 12:00:32 +0300

    added video and @Parcelize annotation to Post data class

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,7 +1,11 @@
 package ru.netology.nmedia.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 const val NON_EXISTING_POST_ID = -1L
 
+@Parcelize
 data class Post(
     val id: Long = NON_EXISTING_POST_ID,
     val author: String = "",
@@ -9,5 +13,6 @@ data class Post(
     val published: String = "",
     var likedByMe: Boolean = false,
     var likes: Int = 0,
-    var shares: Int = 0
-)
+    var shares: Int = 0,
+    var video: String = ""
+) : Parcelable
